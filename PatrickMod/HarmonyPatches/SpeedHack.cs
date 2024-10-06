e692b158c4ee2bc18c7a1ef632b71beda9d5b88a
Allison <108936256+ArchiverXP@users.noreply.github.com>
2024-10-06 15:08:39 -0400

    Add files via upload

---
+++ PatrickMod/HarmonyPatches/SpeedHack.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+
+namespace PatrickMod.HarmonyPatches
+{
+    [HarmonyPatch(typeof(Actor_PlayerCC), "UpdateTargetVelocity")]
+    internal class Actor_PlayerCC_UpdateTargetVelocity
+    {
+        static void Prefix(ref Actor_PlayerCC __instance)
+        {
+            __instance.moveSpeed = 19;
+            __instance.jumpHeight = 19;
+        }
+    }
+}
---
+++ PatrickMod/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace PatrickMod
+{
+    [BepInPlugin("com.archiverxp.patrickmod", "PatrickMod", MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {"com.archiverxp.patrickmod"} is loaded!");
+            var harmony = new Harmony("com.archiverxp.patrickmod");
+            harmony.PatchAll();
+        }
+    }
+}
